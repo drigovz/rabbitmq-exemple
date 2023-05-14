5cb213ce77f3189d4af5bcd0d17c4ad099f011bc
Rodrigo Vaz <rodrigodp2014@gmail.com>
2023-05-14 17:04:00 -0300

    clean producer methods

--- Common/Common/RabbitMq/Producer.cs
+++ Common/Common/RabbitMq/Producer.cs
@@ -8,37 +8,25 @@ public class Producer : IProducer
     {
         _model = connection.CreateModel();
     }
-    
-    private void CreateQueues(string queueName)
-    {
-        _model.QueueDeclare(
-            queue: queueName,
-            durable: false,
-            exclusive: false,
-            autoDelete: false,
-            arguments: null
-        );
-    }
 
-    private void CreateExchange(string exchangeName, string exchangeType)
-    {
+    private void CreateQueues(string queueName) =>
+        _model.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+    private void CreateExchange(string exchangeName, string exchangeType) =>
         _model.ExchangeDeclare(exchange: exchangeName, type: exchangeType);
-    }
 
-    private void BindQueues(string exchangeName, string queueName, string routingKey)
-    {
+    private void BindQueues(string exchangeName, string queueName, string routingKey) =>
         _model.QueueBind(queueName, exchangeName, routingKey);
-    }
-    
+
     public void Send(object message, string exchangeName, string queueName, string routingKey, string exchangeType = ExchangeType.Fanout)
     {
         var serializedMessage = JsonSerializer.Serialize(message);
         var byteMessage = Encoding.UTF8.GetBytes(serializedMessage);
-        
+
         CreateQueues(queueName);
         CreateExchange(exchangeName, exchangeType);
         BindQueues(exchangeName, queueName, routingKey);
-        
+
         _model.BasicPublish(
             exchange: exchangeName,
             routingKey: routingKey,
